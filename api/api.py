ed393059e4cf4f9b0da0b65e5d108d5d59517df9
Camila Moura <camilapaleo@gmail.com>
2019-05-16 16:44:21 +0200

    improvement of file api.py

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 import requests
 from flask import Flask, jsonify
-from flask_restful import Resource, reqparse, Api
+from flask_restful import Resource, reqparse, Api, abort
 
 # Create the application instance
 app = Flask(__name__)
@@ -11,78 +11,83 @@ parser.add_argument('amount')
 parser.add_argument('input_currency')
 parser.add_argument('output_currency')
 
+def currency_converter(amount, input_currency, output_currency=None):
+
+    if output_currency:
+        output_currency = output_currency.upper().strip()
+
+    if not input_currency:
+        return {"error": "Input Currency Required"}, 400
+
+    input_currency = input_currency.upper().strip()
+        
+    if not amount:
+        return {"error": "Invalid Amount"}, 400
+    
+    amount = float(amount)
+
+    if amount <= 0:
+        return {"Bad Request": "Error 400"}
+
+    # Get data currency  
+
+    url = "https://api.exchangerate-api.com/v4/latest/{}".format(input_currency)
+    resp = requests.get(url)
+
+    data = resp.json()
+
+    if resp.status_code != 200:
+        return {"error": "Remote API Server Error"}, resp.status_code
+    
+    # Convert
+
+    if output_currency and output_currency not in data['rates']:
+
+        return {"error": "Invalid Output Currency"}, 400
+
+    elif output_currency:
+
+        return {
+            "input": {
+                "amount": amount,
+                "currency": input_currency,
+            },
+
+            "output": {
+                output_currency: round(data['rates'][output_currency] * amount, 2),
+            }
+        }
+
+    else:
+
+        currencies = data['rates']
+
+        if input_currency in currencies:
+            del currencies[input_currency]
+
+        for output_currency in currencies:
+            new_currencies = [{i: round(v * amount, 2) for i, v in currencies.items()}]
+
+        return {
+            "input": {
+                "amount": amount,
+                "currency": input_currency,
+            },
+            "output": new_currencies
+        }
+
 class CurrencyConverter(Resource):
+   
     def get(self):
 
         args = parser.parse_args()
 
         amount = args.get('amount')        
         input_currency = args.get('input_currency')                
-        output_currency = args.get('output_currency')        
-
-        # report errors args    
-        if output_currency:
-            output_currency = output_currency.upper().strip()
-
-        if not input_currency:
-            return {"error": "input_currency required"}, 400
-
-        input_currency = input_currency.upper().strip()
-         
-        if not amount:
-            return {"error": "invalid amount"}, 400
-        
-        amount = float(amount)
-
-        if amount <= 0:
-            return {"Bad Request": "Error 400"}
-
-        # Get data currency  
-
-        url = "https://api.exchangerate-api.com/v4/latest/{}".format(input_currency)
-        resp = requests.get(url)
-
-        data = resp.json()
-
-        if resp.status_code != 200:
-            return {"error": "Remote API server error"}, resp.status_code
-        
-        # Convert
-
-        if output_currency and output_currency not in data['rates']:
-
-            return {"error": "Invalid output_currencty"}, 400
-
-        elif output_currency:
-
-            return {
-                "input": {
-                    "amount": amount,
-                    "currency": input_currency,
-                },
-
-                "output": {
-                    output_currency: round(data['rates'][output_currency] * amount, 2),
-                }
-            }
-
-        else:
-
-            currencies = data['rates']
-
-            if input_currency in currencies:
-                del currencies[input_currency]
-
-            for output_currency in currencies:
-                new_currencies = [{i: round(v * amount, 2) for i, v in currencies.items()}]
-
-            return {
-                "input": {
-                    "amount": amount,
-                    "currency": input_currency,
-                },
-                "output": new_currencies
-            }
+        output_currency = args.get('output_currency')
+ 
+        return currency_converter(amount, input_currency, output_currency)
+            
 
 api.add_resource(CurrencyConverter, '/currency_converter')
 

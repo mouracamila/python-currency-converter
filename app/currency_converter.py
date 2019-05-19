33149308fa8a7dd4c8288d9ce2a64e0872b2620b
Camila Moura <camilapaleo@gmail.com>
2019-05-19 23:14:41 +0200

    removes space

--- app/currency_converter.py
+++ app/currency_converter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import click
-import requests
 import json
 from api import currency_converter
 
@@ -11,10 +10,9 @@ from api import currency_converter
 @click.option('--input_currency', nargs=1)
 @click.option('--output_currency')
 def main(amount, input_currency, output_currency):
-    resp = currency_converter(amount,input_currency, output_currency)
+    resp = currency_converter(amount, input_currency, output_currency)
     print(json.dumps(resp if isinstance(resp, dict) else resp[0], indent=4))
 
 
 if __name__ == "__main__":
     main()
-    

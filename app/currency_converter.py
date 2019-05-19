8dc8f1806336d04280831800537d6f6dd58508c2
Camila Moura <camilapaleo@gmail.com>
2019-05-19 20:39:21 +0200

    changes parameter name

--- app/currency_converter.py
+++ app/currency_converter.py
@@ -5,10 +5,11 @@ import requests
 import json
 from api import currency_converter
 
+
 @click.command()
 @click.option('--amount', nargs=1, type=float)
-@click.option('--input-currency', nargs=1)
-@click.option('--output-currency')
+@click.option('--input_currency', nargs=1)
+@click.option('--output_currency')
 def main(amount, input_currency, output_currency):
     resp = currency_converter(amount,input_currency, output_currency)
     print(json.dumps(resp if isinstance(resp, dict) else resp[0], indent=4))
@@ -16,3 +17,4 @@ def main(amount, input_currency, output_currency):
 
 if __name__ == "__main__":
     main()
+    

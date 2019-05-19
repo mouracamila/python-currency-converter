b1cea735152612df7dccab0ebbedaa4dc91d1e61
Camila Moura <camilapaleo@gmail.com>
2019-05-19 21:31:08 +0200

    removes space and  checking exit output_currency

--- app/api.py
+++ app/api.py
@@ -13,33 +13,38 @@ parser.add_argument('output_currency')
 
 
 def currency_converter(amount, input_currency, output_currency=None):
-   
+
     input_currency = input_currency.upper().strip()
-    
-    # if input currency code:   
+
+    if output_currency:
+        output_currency = output_currency.upper().strip()
+
+    # if input currency code:
     if not input_currency:
-        return {"error": "Input Currency Required"}, 400    
-  
-    # if input_currency simbol
-    currency_mapping = {'$': 'USD', 'R$': 'BRL', '€': 'EUR', '£': 'GBP', ' ¥': 'JPY', 'CA$': 'CAD', 'Kč': 'CZK'}
-    
+        return {"error": "Input Currency Required"}, 400
+
+    # if input_currency symbol
+    currency_mapping = {
+        '$': 'USD', 'R$': 'BRL', '€': 'EUR', '£': 'GBP',
+        '¥': 'JPY', 'CA$': 'CAD', 'Kč': 'CZK', 'AU$': 'AUD',
+        'HK$': 'HKD', 'MX$': 'MXN', 'NZ$': 'NZD', 'zł': 'PLN',
+        }
+
     if input_currency in currency_mapping:
         input_currency = currency_mapping.get(input_currency)
-    
+
     if output_currency in currency_mapping:
-        output_currency = currency_mapping.get(output_currency)    
-    
+        output_currency = currency_mapping.get(output_currency)
+
     if not amount:
         return {"error": "Invalid Amount"}, 400
-    
+
     amount = float(amount)
 
     if amount <= 0:
         return {"Bad Request": "Error 400"}
 
-
-    # Get data currency  
-
+    # Get data currency
     url = "https://api.exchangerate-api.com/v4/latest/{}".format(
         input_currency)
     resp = requests.get(url)
@@ -48,7 +53,7 @@ def currency_converter(amount, input_currency, output_currency=None):
 
     if resp.status_code != 200:
         return {"error": "Remote API Server Error"}, resp.status_code
-    
+
     if output_currency and output_currency not in data['rates']:
 
         return {"error": "Invalid Output Currency"}, 400
@@ -62,7 +67,8 @@ def currency_converter(amount, input_currency, output_currency=None):
             },
 
             "output": {
-                output_currency: round(data['rates'][output_currency] * amount, 2),
+                output_currency: round(data['rates'][
+                    output_currency] * amount, 2),
             }
         }
 
@@ -74,7 +80,8 @@ def currency_converter(amount, input_currency, output_currency=None):
             del currencies[input_currency]
 
         for output_currency in currencies:
-            new_currencies = [{i: round(v * amount, 2) for i, v in currencies.items()}]
+            new_currencies = [{i: round(
+                v * amount, 2) for i, v in currencies.items()}]
 
         return {
             "input": {
@@ -85,14 +92,14 @@ def currency_converter(amount, input_currency, output_currency=None):
         }
 
 
-class CurrencyConverter(Resource):   
+class CurrencyConverter(Resource):
     def get(self):
 
         args = parser.parse_args()
 
-        amount = args.get('amount')        
-        input_currency = args.get('input_currency')                
-        output_currency = args.get('output_currency') 
+        amount = args.get('amount')
+        input_currency = args.get('input_currency')
+        output_currency = args.get('output_currency')
         return currency_converter(amount, input_currency, output_currency)
 
 
